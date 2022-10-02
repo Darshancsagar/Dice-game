ec86d3644dfb77ce8bcefda5406b213d7d2f3a17
Darshancsagar <109427413+Darshancsagar@users.noreply.github.com>
2022-10-02 12:30:04 +0530

    starting

---
+++ app.js
@@ -0,0 +1,26 @@
+var ran= Math.floor(Math.random()*6)+1;
+
+var randiceimg= "dice"+ran+".png";
+
+var ranimgsrc="images/"+randiceimg;
+ 
+var img1= document.querySelectorAll("img")[0];
+
+setTimeout(1000) 
+
+img1.setAttribute("src", ranimgsrc );
+
+var ran= Math.floor(Math.random()*6)+1;
+
+var randiceimg= "dice"+ran+".png";
+
+var ranimgsrc="images/"+randiceimg;
+ 
+var img1= document.querySelectorAll("img")[1];
+
+img1.setAttribute("src", ranimgsrc );
+
+
+
+
+
